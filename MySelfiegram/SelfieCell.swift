8dc0450635c74ac4c80146716b24bf2f7b775e48
Daniel Mathews <dannyl.mathews@gmail.com>
2016-01-01 20:01:15 -0800

    final

--- MySelfiegram/SelfieCell.swift
+++ MySelfiegram/SelfieCell.swift
@@ -24,9 +24,6 @@ class SelfieCell: UITableViewCell {
                 // This always resets the image to blank, waits for the image to download, and then sets it
                 selfieImageView.image = nil
                 
-                // set it defaulted to false
-                self.likeButton.selected = false
-                
                 let imageFile = post.image
                 imageFile.getDataInBackgroundWithBlock { (data, error) -> Void in
                     if let data = data {
@@ -38,16 +35,22 @@ class SelfieCell: UITableViewCell {
                 usernameLabel.text = post.user.username
                 commentLabel.text = post.comment
                 
+                // set the likeButton defaulted to false
+                likeButton.selected = false
+                
+                // query the likes property on post
                 let query = post.likes.query()
                 query.findObjectsInBackgroundWithBlock({ (users, error) -> Void in
                     
                     if let users = users as? [PFUser]{
                         for user in users {
+                            // If we find that the current user's objectId in our collection
+                            // of likes we set the likeButton to selected
+                            // objectId is a great way to compare if two objects
                             if user.objectId == PFUser.currentUser()?.objectId {
                                 self.likeButton.selected = true
                             }
                         }
-                        
                     }
                 })
                 
@@ -64,44 +67,61 @@ class SelfieCell: UITableViewCell {
         
         // the ! symbol means NOT
         // We are therefore setting the button's selected state to
-        // the opposite of what it was. This is a great way to toggle
+        // the opposite of what it was. This is a great way to toggle buttons
         //
         sender.selected = !sender.selected
         
+        // Get rid of Optionals
         if let post = post,
            let user = PFUser.currentUser() {
             
+            // like button has been selected and we should add a like from currentUser
             if sender.selected {
-                // should add like
                 
+                // PFRelation has a useful method called addObject that adds the unique element
+                // you are passing in as the argument. It will never add multiple copies
+                // of the same element (in this case user)
                 post.likes.addObject(user)
+                
+                // We have modified the likes property on post. We must now save it to Parse
                 post.saveInBackgroundWithBlock({ (success, error) -> Void in
                     if success {
-                        print("likes successfully saved")
-                        self.likeButton.selected = true
+                        print("like from user successfully saved")
+                        
+                        // Creating an row in the Activity table
+                        // Saving it as a "like" type
                         let activity = Activity(type: "like", post: post, user: user)
                         activity.saveInBackgroundWithBlock({ (success, error) -> Void in
-                            print("actvity successfully saved")
+                            print("activity successfully saved")
                         })
+                        
+                        
                     }else{
                         print("error is \(error)")
                     }
                 })
 
                 
-            } else {
-                // should remove like
+            } else { // like button has been deselected and we should remove the like
                 
+                // PFRelation also has a useful method called removeObject that removes 
+                // the element if there is an element to be removed.
                 post.likes.removeObject(user)
                 post.saveInBackgroundWithBlock({ (success, error) -> Void in
                     if success {
-                        print("likes successfully removed")
-                        self.likeButton.selected = false
+                        print("like from user successfully removed")
+                        
+                        //PFQuery to find the like activity
                         if let activityQuery = Activity.query(){
                             activityQuery.whereKey("post", equalTo: post)
+                            activityQuery.whereKey("user", equalTo: user)
                             activityQuery.whereKey("type", equalTo: "like")
                             activityQuery.findObjectsInBackgroundWithBlock({ (activities, error) -> Void in
                                 
+                                
+                                // You should only have one like activity from a user
+                                // but this is code is checking for one or multiple instances
+                                // and deleting all of them
                                 if let activities = activities {
                                     for activity in activities {
                                         activity.deleteInBackgroundWithBlock({ (success, error) -> Void in
@@ -109,16 +129,15 @@ class SelfieCell: UITableViewCell {
                                         })
                                     }
                                 }
-                                
                             })
                         }
+
                     }else{
                         print("error is \(error)")
                     }
                 })
                 
             }
-
         }
         
     }
